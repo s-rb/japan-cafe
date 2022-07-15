625bbbec29fd24541ac8dc047217e8791529ea1f
Roman Surkov <s.rb@list.ru>
2022-07-15 17:25:11 +0300

    Added header, button (cart)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
+import Header from "./components/Layout/Header";
+
 function App() {
     return (
-        <div>
-            <h2>
-                Стартовый проект
-            </h2>
-        </div>
+        <React.Fragment>
+            <Header/>
+        </React.Fragment>
     );
 }
 
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import sushiImage from '../../assets/sushi.jpg';
+import styles from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+    return (
+        <React.Fragment>
+            <header className={styles.header}>
+                <h1>Япона кухня</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={styles['main-image']}>
+                <img src={sushiImage} alt="Блюда японской кухни"/>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Header;

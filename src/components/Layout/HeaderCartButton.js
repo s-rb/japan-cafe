ce7fc778a6cf360f82ad1f24ffe0cf5fe9383e9f
Roman Surkov <s.rb@list.ru>
2022-07-17 20:11:01 +0300

    Добавлен контекст и редъюсер

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
 import CartIcon from "../Cart/CartIcon";
 import styles from './HeaderCartButton.module.css';
+import {useContext} from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+
+    const cartContext = useContext(CartContext);
+
+    const cartItemsNumber = cartContext.items.reduce((currentValue, item) => {
+        return currentValue + item.amount
+    }, 0);
+
     return <button className={styles.button} onClick={props.onClick}>
         <span className={styles.icon}>
             <CartIcon/>
         </span>
         <span>Корзина</span>
         <span className={styles.badge}>
-            2
+            {cartItemsNumber}
         </span>
     </button>
 }
--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,14 +1,44 @@
 import CartContext from "./cart-context";
+import {useReducer} from "react";
 
+const DEFAULT_CART_STATE = {
+    items: [],
+    totalAmount: 0
+}
+
+// Action мы передаем в коде, а стейт - последнее состояние передает реакт
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item); // concat в олтичие от push создаст новый объект массива, а не изменить старый
+        const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    return DEFAULT_CART_STATE;
+}
 
 const CartContextProvider = (props) => {
 
-    const addItemHandler = (item) => {}
-    const removeItemHandler = (id) => {}
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, DEFAULT_CART_STATE);
+
+    const addItemHandler = (item) => {
+        dispatchCartAction({
+            type: 'ADD_ITEM',
+            item: item
+        })
+    }
+    const removeItemHandler = (id) => {
+        dispatchCartAction({
+            type: 'REMOVE_ITEM',
+            id: id
+        })
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount:0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler
     }

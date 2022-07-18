e75178d0fd007b95fa4e1cc4429193162b5f9df1
Roman Surkov <s.rb@list.ru>
2022-07-18 16:58:30 +0300

    Добавление и удаление элементов в корзине кнопками + и -

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,15 +8,15 @@ const Cart = (props) => {
 
     const cartContext = useContext(CartContext);
 
-    const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+    const totalAmount = `$${Math.abs(cartContext.totalAmount).toFixed(2)}`;
     const hasItems = cartContext.items.length > 0;
 
     const addCartItemHandler = (item) => {
-
+        cartContext.addItem({...item, amount: 1})
     }
 
     const removeCartItemHandler = (id) => {
-
+        cartContext.removeItem(id);
     }
 
     // Преконфигурация с помощью bind
